8def13ba83fa7f55a677b17e3622cbb227075e2d
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-10-12 17:17:02 +0800

    Add transaction manager

--- src/main/kotlin/config/ApplicationContext.kt
+++ src/main/kotlin/config/ApplicationContext.kt
@@ -3,10 +3,12 @@ package config
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @ComponentScan(basePackages = ["ga.rugal"])
 @Configuration
 @EnableR2dbcRepositories(basePackages = ["ga.rugal.reactor.core.dao"])
+@EnableTransactionManagement
 open class ApplicationContext {
 }
 
